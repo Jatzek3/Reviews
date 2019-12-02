c4a8a96bed69ab3109f940d3c508a959f6cc11a3
Jacek Kawalec <jokerchocolate@gmail.com>
2019-12-02 17:13:21 +0100

    changed Debug

--- learnpython/settings.py
+++ learnpython/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'u0cekv@c*pcu2j7qt*qe&rl%(+^t4&n(jvuee))unvf_l@enfu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
