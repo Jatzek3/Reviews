947da169fec1a817fb5d2f12ffd90b2b2e56558b
Jacek Kawalec <jokerchocolate@gmail.com>
2019-12-23 13:19:09 +0100

    password reset done - password secure

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,11 @@
 from  django.urls import path
-
+from django.contrib.auth import views
 from .views import SignUpView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('password_reset/', views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
--- learnpython/settings.py
+++ learnpython/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from . import secure
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -132,3 +133,9 @@ LOGOUT_REDIRECT_URL = 'home_index'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = secure.HOST_USER
+EMAIL_HOST_PASSWORD = secure.HOST_PASSWORD
