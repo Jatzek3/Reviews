98eb4f3a8936928a65b2f2fd6d04f05f6434be64
Jacek Kawalec <jokerchocolate@gmail.com>
2020-01-20 13:04:00 +0100

    Security concerns

--- learnpython/secure.py
+++ learnpython/secure.py
@@ -1,2 +1,3 @@
 HOST_USER = 'pythonblogjatzek3@gmail.com'
 HOST_PASSWORD = 'gqeydkobpxomzxox'
+SECRET_KEY = 'u0cekv@c*pcu2j7qt*qe&rl%(+^t4&n(jvuee))unvf_l@enfu'
--- learnpython/settings.py
+++ learnpython/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u0cekv@c*pcu2j7qt*qe&rl%(+^t4&n(jvuee))unvf_l@enfu'
+SECRET_KEY = secure.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
