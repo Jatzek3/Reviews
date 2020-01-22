07cd1752c488df65704006ab5851ecc50019890e
Jacek Kawalec <jokerchocolate@gmail.com>
2020-01-22 17:14:33 +0100

    Deploy with DB configuration OK

--- learnpython/secure.py
+++
@@ -1,7 +0,0 @@
-HOST_USER = 'pythonblogjatzek3@gmail.com'
-HOST_PASSWORD = 'gqeydkobpxomzxox'
-SECRET_KEY = 'u0cekv@c*pcu2j7qt*qe&rl%(+^t4&n(jvuee))unvf_l@enfu'
-
-AWS_ACCESS_KEY_ID = "AKIA4MSN53KNLQDRTHWD"
-AWS_SECRET_ACCES_KEY = "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"
-AWS_STORAGE_BUCKET_NAME = "learn-python-programming"
